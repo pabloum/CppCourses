489044c9e4f82d6d6de29337f6bab38666c8d822
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-05-20 12:31:01 -0500

    Some tests made on pensa portable

--- Test/denkenCeluco/main.cpp
+++ Test/denkenCeluco/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "interval_map.h"
 
+#include <Climits>
+
+
 void print_lowest();
 
 int main(int argc, char const *argv[]) {
@@ -17,7 +20,15 @@ int main(int argc, char const *argv[]) {
   // interval_map <int, char> myIntCharMap ('F');
   // myIntCharMap.assign(-2147483645,-2147483641,'P');
   // myIntCharMap.show();
+  std::printf("El char más bajo: %c\n", std::numeric_limits<char>::lowest() + 128 + 65);
 
+  if (std::numeric_limits<char>::lowest() == WCHAR_MIN) {
+    std::cout << "Lowest char is equal to  WCHAR_MIN" << '\n';
+  } else if (std::numeric_limits<char>::lowest() == CHAR_MIN) {
+    std::cout << "Lowest char is equal to  CHAR_MIN" << '\n';
+  } else if(WCHAR_MIN == CHAR_MIN) {
+    std::cout << "WCHAR_MIN == CHAR_MIN" << '\n';
+  }
   // print_lowest();
   return 0;
 }
