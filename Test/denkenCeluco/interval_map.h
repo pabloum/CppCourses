6936fb78bceae8ccfabdc0aaddb8acb0ea803e9a
PABLO <pablo.um00@gmail.com>
2020-05-24 21:54:54 -0500

    Clean up old solution and archive it.

--- Test/denkenCeluco/interval_map.h
+++ Test/denkenCeluco/interval_map.h
@@ -26,55 +26,10 @@
           // INSERT YOUR SOLUTION HERE
 
           // First, I check whether the interval is empty or not.
-          // If it is empty or if the value is lower than the lowest possible value, the function terminates
-          if (!(keyBegin < keyEnd) || keyBegin < std::numeric_limits<K>::lowest()) {
+          if (!(keyBegin < keyEnd)) {
               return;
           }
 
-          auto itComponentBegin = m_map.find(keyBegin);
-          auto itComponentEnd = m_map.find(keyEnd);
-
-          //--------------------------
-          //--------------------------
-          // checking if the key already exists
-          if (m_map.count(keyEnd) >= 1 || m_map.count(keyBegin) >= 1) {
-
-              // checking that the new value is not equal to the next one or to the previous.
-              // We need to preserve the solution canonical
-              if  (
-                    ((std::prev(itComponentBegin))->second == val) ||
-                    (itComponentEnd->second == val)
-                  )
-              {
-                  return; // Just terminate the function
-              }
-          }
-
-          //--------------------------
-          //--------------------------
-          // Completes till new kyeBegin with prevous end value
-          if (itComponentBegin == m_map.end()) {
-              for (K key = m_map.rbegin()->first; key < keyBegin; key++) {
-                  m_map.insert(m_map.end(), std::make_pair(key, m_map.rbegin()->second));
-              }
-          }
-
-          //--------------------------
-          //--------------------------
-          // Fills new interval
-          // TODO -> change this for to iterators upper_bound(); --> cHECK it out
-          // for (auto it = itComponentBegin; it != itComponentEnd; it++) {
-          for (K key = keyBegin; key < keyEnd; key++) {
-              // Checks if the key is new or if it should Overwrite
-              auto itComponent = m_map.find(key);
-
-              if (itComponent == m_map.end()) {
-                  m_map.insert(m_map.end(), std::make_pair(key, val));
-              }
-              else {
-                  itComponent->second = val;
-              }
-          }
       }
 
       // look-up of the value associated with key
