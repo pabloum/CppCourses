e2151fa955af62a8faf2b3b6c00966f052752921
Pablo Uribe Montoya <pablo.um00@gmail.com>
2020-06-09 13:13:45 -0500

    Add utility folder, which will contain initially a Matrix class This class will be implemented in C++ and Python. At this point, there is only the skeleton.

---
+++ Utility/LinearAlgebra/Matrix.h
@@ -0,0 +1,18 @@
+#ifndef _MATRIX_HPP_
+#define _MATRIX_HPP_
+
+#include <functional>
+#include <vector>
+#include <map>
+
+namespace LINEAR_ALGEBRA{
+
+  template <typename T>
+  class Matrix {
+    public:
+    private:
+  };
+
+}
+
+#endif /*_MATRIX_HPP_*/
---
+++ Utility/LinearAlgebra/pythonDraft/Matrix.py
@@ -0,0 +1,7 @@
+
+class Matrix():
+    """docstring for Matrix."""
+
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
